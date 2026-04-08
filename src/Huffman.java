4835b4cc11edb46d15fc9daa60dde87548798807
JustinHo09 <hojustin09@gmail.com>
2026-04-07 23:22:00 -0500

    I finished the frequency table printing and made a reverse numToChar method.
    I made a reverse of the charToNum method being the numToChar method which now takes in an integer and returns its associated character. This is then used in the frequency table print which prints out the character and its frequency only if it appears in the string meaning it has a frequency count of 0.

--- src/Huffman.java
+++ src/Huffman.java
@@ -75,8 +75,73 @@ public class Huffman {
         return -1;
     }
 
+    public char numToChar(int i){
+        if(i == 0){
+            return ' ';
+        }else if(i == 1){
+            return 'a';
+        }else if(i == 2){
+            return 'b';
+        }else if(i == 3){
+            return 'c';
+        }else if(i == 4){
+            return 'd';
+        }else if(i == 5){
+            return 'e';
+        }else if(i == 6){
+            return 'f';
+        }else if(i == 7){
+            return 'g';
+        }else if(i == 8){
+            return 'h';
+        }else if(i == 9){
+            return 'i';
+        }else if(i == 10){
+            return 'j';
+        }else if(i == 11){
+            return 'k';
+        }else if(i == 12){
+            return 'l';
+        }else if(i == 13){
+            return 'm';
+        }else if(i == 14){
+            return 'n';
+        }else if(i == 15){
+            return 'o';
+        }else if(i == 16){
+            return 'p';
+        }else if(i == 17){
+            return 'q';
+        }else if(i == 18){
+            return 'r';
+        }else if(i == 19){
+            return 's';
+        }else if(i == 20){
+            return 't';
+        }else if(i == 21){
+            return 'u';
+        }else if(i == 22){
+            return 'v';
+        }else if(i == 23){
+            return 'w';
+        }else if(i == 24){
+            return 'x';
+        }else if(i == 25){
+            return 'y';
+        }else if(i == 26){
+            return 'z';
+        }
+        return '!';
+    }
+
+
     public void printStats(){
-        
+        System.out.println("Frequencies:");
+        for(int i=0; i<frequency.length;i++){
+            if(frequency[i] >0){
+                System.out.println(numToChar(i)+ " : " +frequency[i]);
+            }
+        }
     }
 
 }
