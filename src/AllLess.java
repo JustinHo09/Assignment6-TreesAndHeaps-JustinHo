cf0277386d204d34b6b33aca6ad2d118b9c0f574
JustinHo09 <hojustin09@gmail.com>
2026-04-08 22:02:06 -0500

    I created a main method with a test case, and I started on the algorithm.
    I created the main method which just has a test case, which will eventually be used on the finished algorithm. Then I made the base case for the algorithm which is to just check if the
    length is 0 and to just print emepty brackeets and reeturn.

--- src/AllLess.java
+++ src/AllLess.java
@@ -1,2 +1,18 @@
 public class AllLess {
+
+    public static void main(String[] args){
+        String[] input = {"zero","size","nutella","jojo","luna","isse","astor",
+                "as","entretien","","cal"};
+        int x = 3;
+    }
+
+    public static void printLess(String[] input, int max){
+        if(input.length == 0){
+            System.out.println("[]");
+            return;
+        }
+
+        
+    }
+
 }
