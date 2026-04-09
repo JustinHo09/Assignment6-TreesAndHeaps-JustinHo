9c89bc78bef6084479739c2a4ebabc5472ac5e54
JustinHo09 <hojustin09@gmail.com>
2026-04-08 22:25:09 -0500

    I made the basic consructor and member variable data for the BRNode.
    I made the basic constructor and member data variables for the BRNode like data, children, parent, and color, where true is black and false is red.

--- src/BRNode.java
+++ src/BRNode.java
@@ -1,2 +1,16 @@
 public class BRNode {
+
+    private int data;
+
+    private BRNode left;
+    private BRNode right;
+    private BRNode parent;
+
+    // True is black and False is red
+    private boolean color;
+
+    public BRNode(int data){
+        this.data = data;
+        color = false;
+    }
 }
