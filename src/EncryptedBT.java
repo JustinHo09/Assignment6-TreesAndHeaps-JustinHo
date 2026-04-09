b67ad582f8efd349456fab2dfb5f9a1cb2e0dd0e
JustinHo09 <hojustin09@gmail.com>
2026-04-08 21:29:29 -0500

    I made the main method with a test input to test the algorithm once complete and I started the initial logic for the method.
    I made the main method with one input case that I will use to test my algorithm once done. Then I started the early logic for my code. I started the logic of first seeing if the tree is empty or null and returning false since it would be impossible for that to have a value. Then it also checks if the root is -1, which would violate the unencrypted rquirement thus its not possible for the algoirthm to unencrypt it properly. Then I made another array that the same size as the input to store the decoded values, and after that I follow the ruling and set the root, index 0, to be 1.

--- src/EncryptedBT.java
+++ src/EncryptedBT.java
@@ -1,2 +1,18 @@
 public class EncryptedBT {
+
+    public static void main(String[] args){
+        int[] bt = {-2,-2,-1,-2,-1};
+        int t=1;
+    }
+
+    public static boolean isInBT(int[] input, int target){
+
+        // Enpty or null tree or doesn't meet thee condition that root = -1
+        if(input.length == 0 || input == null || input[0] == -1){
+            return false;
+        }
+
+        int[] unencryped = new int[input.length];
+        unencryped[0] = 1;
+    }
 }
