93878d427bab58c2f42b50e407ca5b5e76d7e304
JustinHo09 <hojustin09@gmail.com>
2026-04-08 22:41:42 -0500

    I made the constructors for the tree, made a method to get the root, made the header for the insert and remove methods, and made an array to tree method.
    I made two constructors, one takes in a node and makes it teh root, the other takes in an array and calls the arrayToBRTree method which takes the array and makes it into a BRTree using the insertNode method insidde of a loop. Then I made a method to return the root. THen I started on the head for the insert and remove node methods. For the insert I made the base case of the root being null and to just make that node the new one. I added comments to the bodies of both method to plan what to do next.

--- src/BRTree.java
+++ src/BRTree.java
@@ -1,4 +1,40 @@
 public class BRTree {
 
     BRNode root;
+
+    public BRTree(BRNode root){
+        this.root = root;
+    }
+
+    public BRTree(int[] array){
+        arrayToBR(array);
+    }
+
+    public BRNode getRoot(){
+        return root;
+    }
+
+    public void insertNode(BRNode node){
+        if(root == null){
+            root = node;
+            root.changeColor();
+        }
+        // Find where to add and add
+
+        //Rebalance if needed
+    }
+
+    public void removeNode(int value){
+        // Search for node with that value
+
+        //Go through 6 cases
+
+        // Do bst remove
+    }
+
+    public void arrayToBR(int[] array){
+        for(int i=0; i< array.length;i++){
+            insertNode(new BRNode(array[i]));
+        }
+    }
 }
