6bddbbf1da42f07597d92d60c47e23c16e7644d9
JustinHo09 <hojustin09@gmail.com>
2026-04-08 22:15:32 -0500

    I added the method to the main method, and I finished the algorithm's searching and printing.
    I added the algorithm to the main method so it can be tested. I also finished the algorithm by going through the entire input and checking the length of the string at the index, from 0 to input.length, and seeing if it less than the target. If so it will print it inparentheses. To get the format right I have print stameent before the loop with an open bracket and one after with teh ckosing one. Then to get the comma's right, i made a boolean first be true, which will not print a , if it is the first valid value. This is set to false at the end of one iteration of the forloop if the coniditon is meet, which means that all middle and end ones will have a comma before them, giving it the proper format.

--- src/AllLess.java
+++ src/AllLess.java
@@ -4,15 +4,37 @@ public class AllLess {
         String[] input = {"zero","size","nutella","jojo","luna","isse","astor",
                 "as","entretien","","cal"};
         int x = 3;
+        printLess(input,x);
     }
 
     public static void printLess(String[] input, int max){
+
         if(input.length == 0){
             System.out.println("[]");
             return;
         }
 
-        
+        boolean first = true;
+
+        System.out.print("[");
+        for(int i=0; i<input.length; i++){
+
+            String current = input[i];
+
+            if(current.length() < max){
+
+                if(!first) {
+                    System.out.print(", ");
+                }
+
+                System.out.print("\"");
+                System.out.print(current);
+                System.out.print("\"");
+
+                first = false;
+            }
+        }
+        System.out.print("]\n");
     }
 
 }
