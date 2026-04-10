41e02ca9d0ab65b9c920e05df7d1cc85ef787555
JustinHo09 <hojustin09@gmail.com>
2026-04-09 22:08:22 -0500

    I implemented rotations for the BRTree.
    I implement left and right rotation methods for the BRTree class for problem 6.

--- src/BRTree.java
+++ src/BRTree.java
@@ -37,4 +37,55 @@ public class BRTree {
             insertNode(new BRNode(array[i]));
         }
     }
+
+    public void balance(BRNode node){
+
+    }
+
+    public void rotateRight(BRNode node){
+        BRNode left = node.getLeft();
+        BRNode parent = node.getParent();
+
+        // set its left's right to be its left
+        node.setLeft(left.getRight());
+        if(left.getRight() != null) {
+            left.getRight().setParent(node);
+        }
+
+        // set it to be its left's right
+        left.setRight(node);
+        node.setParent(left);
+        left.setParent(parent);
+
+        // update parent if exists
+        if(parent == null){
+            root = left;
+        }else if(parent.getLeft() == node){
+            parent.setLeft(left);
+        }else{
+            parent.setRight(left);
+        }
+    }
+
+    public void rotateLeft(BRNode node){
+        BRNode right = node.getRight();
+        BRNode parent = node.getParent();
+
+        node.setRight(right.getLeft());
+        if(right.getLeft() != null){
+            right.getLeft().setParent(node);
+        }
+
+        right.setLeft(node);
+        node.setParent(right);
+        right.setParent(parent);
+
+        if(parent == null){
+            root = right;
+        }else if(parent.getRight() == node){
+            parent.setRight(right);
+        }else{
+            parent.setLeft(right);
+        }
+    }
 }
