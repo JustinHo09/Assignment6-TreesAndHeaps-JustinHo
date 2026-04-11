599b466da30c4ebc79b3025799fa4bf50c072466
JustinHo09 <hojustin09@gmail.com>
2026-04-10 21:18:55 -0500

    I finished the HeapNode class for problem 3 adding constructors for the leaves and internal nodes, and the get methods.
    I added the appropriate member data variables needed for this being left, right, data, and frequency. I also made two constructors, one for the leaves containing the target letters, and one for the internal nodes which hold frequency. The internal node's can't be one of the valid characters, so I gave them all ! as a design choice. Then I implemented the get methods for all of the member data variabes so it can be accessed.

--- src/HeapNode.java
+++ src/HeapNode.java
@@ -1,4 +1,35 @@
 public class HeapNode {
 
-    
+    public char data;
+    public HeapNode left;
+    public HeapNode right;
+    public int frequency;
+
+    public HeapNode(char letter, int freq){
+        data = letter;
+        frequency = freq;
+    }
+
+    public HeapNode(HeapNode l, HeapNode r){
+        left = l;
+        right = r;
+        data = '!';
+        frequency = l.getFrequency() + r.getFrequency();
+    }
+
+    public int getFrequency(){
+        return frequency;
+    }
+
+    public HeapNode getLeft(){
+        return left;
+    }
+
+    public HeapNode getRight(){
+        return right;
+    }
+
+    public char getData(){
+        return data;
+    }
 }
