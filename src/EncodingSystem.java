ac64c0a5ba902915aedc3834da30cf75ab911a68
JustinHo09 <hojustin09@gmail.com>
2026-04-11 19:51:42 -0500

    I added a main method to test the class, a constructor to initilizes the set of texts to be encoded, encode those texts, create an instance of the Huffman class, and have those as member variablle data too.
    I created a main method which as a set to texts to be used as the input. Then I created a constructor for the EncodingSystem class, which initilizes  an instance of th Huffman class, initlizese the texts array to be thee input texts, and initilizes an array of the encoded texts to be thee same size as the input one, and then fill it with the encoded texts; I also have the huffman instance, input array, and encoded array. as member variable data.

--- src/EncodingSystem.java
+++ src/EncodingSystem.java
@@ -1,3 +1,24 @@
 public class EncodingSystem {
-    
+
+    public Huffman huff;
+    public String[] inputs;
+    public String[] encoded;
+
+    public static void main(String[] args){
+        String[] texts = {"marcus fenix is a gear"};
+        EncodingSystem test = new EncodingSystem(texts);
+    }
+
+    public EncodingSystem(String[] texts){
+        huff = new Huffman();
+        inputs = texts;
+        encoded = new String[texts.length];
+
+        // Fill the encoded array with all of the encoded strings
+        for(int i=0; i< texts.length; i++){
+            huff.frequencyCount(texts[i]);
+            huff.buildHuffman();
+            encoded[i] = huff.encode(texts[i]);
+        }
+    }
 }
