9dcf51a78687d7b93f6b4d3f9b5662d625dfcee1
JustinHo09 <hojustin09@gmail.com>
2026-04-12 15:15:02 -0500

    I fixed some typos and null pointer exceptions in problem 4.
    I found a case that might lead to a null pointer eception if the input was null, and I fixed some typos i had.

--- src/EncryptedBT.java
+++ src/EncryptedBT.java
@@ -11,22 +11,22 @@ public class EncryptedBT {
 
         int[] bt3 = {-2,-1,-2,-2,-1};
         t=4;
-        System.out.println(isInBT(bt2,t));
+        System.out.println(isInBT(bt3,t));
     }
 
     public static boolean isInBT(int[] input, int target){
 
         // Enpty or null tree or doesn't meet the condition that root = -1
-        if(input.length == 0 || input == null || input[0] == -1){
+        if(input == null || input.length == 0 || input[0] == -1){
             return false;
         }
-
-        int[] unencryped = new int[input.length];
-        unencryped[0] = 1;
 
         if(target == 1){
             return true;
         }
+
+        int[] unencrypted = new int[input.length];
+        unencrypted[0] = 1;
 
         int left;
         int right;
@@ -36,13 +36,14 @@ public class EncryptedBT {
         for(int i=0; i<input.length; i++){
             // only decode if the input at the index is valid
             // and its parent is a valid node.
-            if(input[i] == -2 && unencryped[i] != 0){
-                x = unencryped[i];
+            if(input[i] == -2 && unencrypted[i] != 0){
+                x = unencrypted[i];
+
                 // check the left
                 left = 2 * i + 1;
                 if(left < input.length && input[left] == -2){
-                    unencryped[left] = 3 * x +1;;
-                    if(unencryped[left] == target){
+                    unencrypted[left] = 3 * x +1;
+                    if(unencrypted[left] == target){
                         return true;
                     }
                 }
@@ -50,15 +51,14 @@ public class EncryptedBT {
                 //check the right
                 right = 2 * i +2;
                 if(right < input.length && input[right] == -2){
-                    unencryped[right] = 2  * x + 5;
-                    if(unencryped[right] == target){
+                    unencrypted[right] = 2  * x + 5;
+                    if(unencrypted[right] == target){
                         return true;
                     }
                 }
 
             }
         }
-
         return false;
     }
 }
